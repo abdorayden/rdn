5205fa37c6f08449edf6e771137b84889d6cdf75
abdorayden <abdenoursouane025@gmail.com>
2026-05-19 20:29:25 +0100

    before fixing the bugs

--- embed/main.c
+++ embed/main.c
@@ -1,11 +1,51 @@
 #include <stdio.h>
 #include "../include/src.h"
 
+// GCC is ALL u neeeeed
+// built: gcc -o main main.c && ./main
+
 void hello (RDNState* state) {
     Value* top = ray_pop(state);
     if (top->type == VALUE_INTEGER) {
         printf("%ld\n" , top->as.integer);
     }
+}
+
+bool f(RDNApi *api){
+    /*
+     * Native list pattern:
+     * 1. push the result list
+     * 2. push a temporary value
+     * 3. append that value into the list
+     * 4. pop the temporary value, because list_append clones it
+     *
+     * After this function returns, only the list should remain on the stack.
+     */
+    if (!api->push_list(api)) {
+        return false;
+    }
+
+    if (!api->push_string(api, "10")) {
+        return false;
+    }
+    if (!api->list_append(api, -2, -1)) {
+        return false;
+    }
+    if (!api->pop(api, 1)) {
+        return false;
+    }
+
+    if (!api->push_string(api, "20")) {
+        return false;
+    }
+    if (!api->list_append(api, -2, -1)) {
+        return false;
+    }
+    if (!api->pop(api, 1)) {
+        return false;
+    }
+
+    return true;
 }
 
 int main(void)
@@ -17,9 +57,11 @@ int main(void)
     Vars vars = {0};
     Funcs funcs = {0};
     ray_append(&funcs, create_func_entry("hey", "331 print"));
-    evaluate_source(&stack, &vars, &funcs, "hey call");
+    ray_append(&funcs, create_native_func_entry("foo", f, NULL));
     evaluate_source(&stack, &vars, &funcs, "1");
     hello(&stack);
+    evaluate_source(&stack, &vars, &funcs, "hey call");
+    evaluate_source(&stack, &vars, &funcs, "foo call print");
 
     return 0;
 }
--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 
 // TODO: make a good api in C so i can implement native functions or create bindings
 // TODO: introduce big ints 
+// TODO: introduce null type
 
+// TODO: finish std native and nonative libs
 
 int main(int argc, char **argv) {
     return rdn_main(argc, argv);
